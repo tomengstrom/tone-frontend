c85a4c799cd1380ef06bc7d7e8c637b815fcbb8e
Tom Engström <tom@nemesol.fi>
2018-02-12 16:53:12 +0200

    some updates

--- main.js
+++ main.js
@@ -9,28 +9,33 @@ canvas.height = window.innerHeight;
 
 context.lineWidth = radius*2;
 
-var putPoint = function(e){
+var putPoint = function(e) {
     if(dragging) {
-    context.lineTo(e.clientX, e.clientY);
-    context.stroke();
-    context.beginPath();
-    context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
-    context.fill();
-    context.beginPath();
-    context.moveTo(e.clientX, e.clientY);
-}
+
+      context.lineTo(e.clientX, e.clientY);
+      context.stroke();
+      context.beginPath();
+      context.arc(e.clientX, e.clientY, radius, 0, Math.PI*2);
+      context.fill();
+      context.beginPath();
+      context.moveTo(e.clientX, e.clientY);
+
+    }
 }
 
-var engage = function(e){
+var engage = function(e) {
     dragging = true;
+    context.beginPath();
+    currentPath = {};
     putPoint(e);
 }
 
 var disengage = function(){
     dragging = false;
-    context.beginPath();
+    savePath();
 }
 
 canvas.addEventListener('mousedown', engage);
+
 canvas.addEventListener('mousemove', putPoint);
 canvas.addEventListener('mouseup', disengage);
